c305376f682f653927f2c781743303aa27919700
M. Yasir Irfan <136885436+cleverdeve@users.noreply.github.com>
2024-12-21 01:26:46 -0800

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+const accountId ="12345"
+const ag ="2021-ag-5889"
+let firstcode ="Hello World"
+let myname ="Yasir Irfan"
+let secondcode;
+city="Toba Tek Singh"
+
+console.table([accountId, ag, firstcode, myname, secondcode, city])
